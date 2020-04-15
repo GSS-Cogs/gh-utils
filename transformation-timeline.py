15f6edfbbd0c05bacf3295a133cdcb576e00b452
Alex Tucker <alex@floop.org.uk>
2020-04-15 10:02:14 +0100

    Catch some JSON parse exceptions.

--- transformation-timeline.py
+++ transformation-timeline.py
@@ -2,6 +2,7 @@ from functools import lru_cache
 
 from github import Github, UnknownObjectException, GithubException
 import json
+from json.decoder import JSONDecodeError
 
 with open('.token') as tf:
     token = tf.readline()
@@ -16,19 +17,29 @@ def get_project(id):
     return g.get_project(id)
 
 
+
 for family in family_repos:
     try:
         ds_info = json.loads(family.get_contents('datasets/info.json').decoded_content)
     except GithubException:
         print(f'{family.name} has no datasets/info.json file')
         continue
+    except JSONDecodeError:
+        print(f'{family.name} problem parsing JSON in datasets/info.json file')
+        continue
     for pipeline in ds_info['pipelines']:
         try:
             pipeline_info = json.loads(family.get_contents(f'datasets/{pipeline}/info.json').decoded_content)
-            pipeline_issue_no = pipeline_info['transform']['main_issue']
-            pipeline_issue = family.get_issue(pipeline_issue_no)
+            if 'transform' in pipeline_info and 'main_issue' in pipeline_info['transform']:
+                pipeline_issue_no = pipeline_info['transform']['main_issue']
+                pipeline_issue = family.get_issue(pipeline_issue_no)
+            else:
+                continue
         except GithubException:
-            print(f'{pipeline} has no info.json file')
+            print(f'{family.name} has no datasets/{pipeline}/info.json file')
+            continue
+        except JSONDecodeError:
+            print(f'{family.name} problem parsing datasets/{pipeline}/info.json file')
             continue
         for event in pipeline_issue.get_timeline():
             print(f'{pipeline}: {event.event} {event.created_at}')
